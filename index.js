84d9415a539b8b0a35d5cf24f7d1a5f01242703a
jasmosez <jas.schaffer@gmail.com>
2020-01-04 22:33:07 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here
+let var1 = 31;
+let var2 = 2;
+
+let multiply = var1 * var2;
+
+let random = Math.ceil(Math.random * 10);
+
+let mod = 9 % 5;
+
+let max = Math.max(1, 15, 20);
